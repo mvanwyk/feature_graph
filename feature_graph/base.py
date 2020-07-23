63fab0e9f967edb67b6608e86859751b43c06684
Murray Vanwyk <murray.vanwyk@gmail.com>
2020-07-23 14:50:56 +0200

    Added DAG state storage

--- feature_graph/base.py
+++ feature_graph/base.py
@@ -1,9 +1,8 @@
 import contextvars
-import uuid
 from typing import List, Union
 from graphviz import Digraph
-
-# TODO: Implement caching
+from sqlitedict import SqliteDict
+from hashlib import md5
 
 __dag = contextvars.ContextVar("dag")
 
@@ -20,10 +19,13 @@ def set_dag(dag):
 
 
 class FeatureDAG:
-    def __init__(self, dag_params: dict = None):
+    def __init__(self, dag_params: dict = None, state_db: str = ":memory:"):
         self._nodes = set()
         self._dot = None
         self._diag_params = dag_params
+        self._state_dict = SqliteDict(
+            state_db, autocommit=True, encode=str, decode=str, tablename="state"
+        )
 
     @property
     def dag_params(self):
@@ -39,23 +41,43 @@ class FeatureDAG:
         self.print_graph()
 
     def add_node(self, node: "FeatureNode"):
+
+        if node in self._nodes:
+            raise ValueError("The same node can't be added to the DAG twice")
+        if node.name in [n.name for n in self._nodes]:
+            raise ValueError("Two nodes can't have the same name")
+
         self._nodes.add(node)
+
+    def compact_state(self):
+        """Removes any nodes in the state that aren't in the DAGs current list of nodes
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
+        dag_node_ids = [n.node_id for n in self._nodes]
+        for node_id in self._state_dict.keys():
+            if node_id not in dag_node_ids:
+                del self._state_dict[node_id]
 
     def run_feature_graph(self):
 
-        # Get nodes without parents
         nodes_no_children = [node for node in self._nodes if len(node.children) == 0]
 
         for node in nodes_no_children:
             self._walk_graph_query(parent_nodes=node.parents)
-            if not node.query_ran:
+            if node.is_node_stale:
                 node.run()
 
     def _walk_graph_query(self, parent_nodes: List["FeatureNode"]):
 
         for node in parent_nodes:
             self._walk_graph_query(parent_nodes=node.parents)
-            if not node.query_ran:
+            if node.is_node_stale:
                 node.run()
 
     # Printing the graph ------------------------------------------------------
@@ -64,7 +86,6 @@ class FeatureDAG:
 
         self._dot = Digraph()
 
-        # Get nodes without parents
         nodes_no_parents = []
         for node in self._nodes:
             self._dot.node(node.node_id, node.name)
@@ -97,11 +118,10 @@ class FeatureDAG:
 class FeatureNode:
     def __init__(self, name: str):
 
-        self._node_id = str(uuid.uuid4())
-        self._name = name
+        self._name = name.strip()
+        self._node_id = md5(self._name.lower().encode("utf-8")).hexdigest()
         self._parents = set()
         self._children = set()
-        self._query_ran = False
 
         self._dag = get_dag()
         if self._dag is None:
@@ -122,14 +142,107 @@ class FeatureNode:
 
     @property
     def node_id(self):
+        """Returns the node_id
+
+        The node_id is the md5 hash of the nodes name after being stripped and
+        converted to lower case. It used by the internals of feature_graph and
+        uniquely identifies a node.
+
+        Args:
+            None
+
+        Returns:
+            str: A unique ID used to identify a node
+
+        """
         return self._node_id
 
     @property
-    def query_ran(self) -> bool:
-        return self._query_ran
+    def is_node_stale(self) -> bool:
+        """Used to check if the node needs to be run
+
+        Args:
+            None
+
+        Returns:
+            bool: True if the state cache tag DOES NOT equal the node's current cache
+            tag, False otherwise.
+
+        """
+        return not self._calc_current_cache_tag() == self._get_state_cache_tag
+
+    def _calc_current_cache_tag(self) -> str:
+        """Calculates the current state of the node and returns as a string
+
+        This function should be generally be overridden by a subclass. It's purpose is
+        to return a string (eg a hex md5 hash) that represents the state of all the
+        inputs to the node. For instance, if the node was processing a file it might
+        check the last modified date of the file, hash it and return that as a hex
+        string. By comparing the this cache tag with the one in the state it can be
+        determined if the node needs to be rerun.
+
+        Args:
+            None
+
+        Returns:
+            str: A string representing the state of all inputs to a node
+
+        """
+        return "True"
+
+    @property
+    def _get_state_cache_tag(self) -> str:
+        """Returns the cache tag in the DAG's state
+
+        Args:
+            None
+
+        Returns:
+            str: The cache tag for the node in the DAG's cache
+
+        """
+        return self._dag._state_dict.get(self.node_id, None)
+
+    def _set_state_cache_tag(self, cache_tag: str):
+        """Internal function that sets the cache tag for the node
+
+        Args:
+            cache_tag (str): The second parameter.
+
+        Returns:
+            None
+
+        """
+        self._dag._state_dict[self.node_id] = cache_tag
+
+    def clear_state(self):
+        """Clears the cache tag from the DAG's state
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
+        if self.node_id in self._dag._state_dict.keys():
+            del self._dag._state_dict[self.node_id]
 
     def run(self):
-        self._query_ran = True
+        """Internal function that sets the cache tag for the node
+
+        This function should be overridden by a subclass but the call must be passed
+        to the super to ensure that the cache state is properly maintained. eg,
+        `super(MySubClass, self).run()`
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
+        self._set_state_cache_tag(cache_tag=self._calc_current_cache_tag())
 
     def __rshift__(self, other: Union["FeatureNode", List["FeatureNode"]]):
         if not isinstance(other, list):
--- tests/test_feature_graph.py
+++ tests/test_feature_graph.py
@@ -1,6 +1,7 @@
 from feature_graph import __version__
 from feature_graph.base import FeatureDAG, FeatureNode
 import pytest
+import os
 
 
 def test_version():
@@ -113,7 +114,7 @@ def test_assign_parent_to_node_fail():
 def test_no_dag_setup():
 
     with pytest.raises(EnvironmentError):
-        a = FeatureNode(name="query a")  # noqa: F841
+        _ = FeatureNode(name="query a")
 
 
 def test_node_cant_be_assigned_twice():
@@ -125,3 +126,90 @@ def test_node_cant_be_assigned_twice():
         a >> b
         with pytest.raises(ValueError):
             a >> b
+
+
+def test_two_nodes_cant_have_same_name():
+
+    with FeatureDAG():
+        _ = FeatureNode(name="query")
+        with pytest.raises(ValueError):
+            _ = FeatureNode(name="query")
+
+
+def test_same_node_cant_be_added_to_dag_twice():
+
+    with FeatureDAG() as dag:
+        a = FeatureNode(name="query")
+
+    with pytest.raises(ValueError):
+        dag.add_node(a)
+
+
+def test_compact_state():
+
+    with FeatureDAG() as dag:
+        a = FeatureNode(name="query a")
+
+    dag.run_feature_graph()
+
+    dag._nodes.remove(a)
+
+    assert a.node_id in dag._state_dict.keys()
+
+    dag.compact_state()
+
+    assert a.node_id not in dag._state_dict.keys()
+
+
+def test_state_stored():
+
+    with FeatureDAG() as dag:
+        a = FeatureNode(name="query a")
+
+    assert a.is_node_stale is True
+
+    dag.run_feature_graph()
+
+    assert a.is_node_stale is False
+
+
+def test_clear_state():
+
+    with FeatureDAG() as dag:
+        a = FeatureNode(name="query a")
+
+    assert a._get_state_cache_tag is None
+
+    dag.run_feature_graph()
+
+    assert a._get_state_cache_tag is not None
+
+    a.clear_state()
+
+    assert a._get_state_cache_tag is None
+
+
+def test_state_stored_with_file():
+
+    state_db = "./dag_state.sqlite"
+    if os.path.exists(state_db):
+        os.remove(state_db)
+
+    with FeatureDAG(state_db=state_db) as dag:
+        a = FeatureNode(name="query a")
+
+    assert a.is_node_stale is True
+
+    dag.run_feature_graph()
+
+    assert a.is_node_stale is False
+
+    del a
+    del dag
+
+    with FeatureDAG(state_db=state_db):
+        a = FeatureNode(name="query a")
+
+    assert a.is_node_stale is False
+
+    os.remove(state_db)
